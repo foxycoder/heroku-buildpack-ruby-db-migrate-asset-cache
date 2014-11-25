11cfbc2eae6e91549a6043699655b89e47897c6e
Wouter de Vos <wouter@springest.com>
2014-11-26 00:07:16 +0100

    Show contents of working dir.

--- lib/language_pack/ruby.rb
+++ lib/language_pack/ruby.rb
@@ -533,6 +533,7 @@ WARNING
           instrument "ruby.bundle_install" do
             bundle_time = Benchmark.realtime do
               bundler_output << pipe("apt-get update && apt-get install cmake -y", out: "2>&1", env: env_vars, user_env: true)
+              bundler_output << pipe("ls -la", out: "2>&1", env: env_vars, user_env: true)
               bundler_output << pipe("#{bundle_command} --no-clean", out: "2>&1", env: env_vars, user_env: true)
             end
           end
